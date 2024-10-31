76f3007285cf75b5edf433eaa9664764e0497a1d
Divam <dfordivam@gmail.com>
2024-10-31 13:04:39 +0900

    fix test for long_tx

--- ragger-tests/test_sign_cmd.py
+++ ragger-tests/test_sign_cmd.py
@@ -65,40 +65,30 @@ def test_sign_tx_short_tx(backend, scenario_navigator, firmware, navigator):
 def test_sign_tx_long_tx(backend, scenario_navigator, firmware, navigator):
     # Use the app interface instead of raw interface
     client = BoilerplateCommandSender(backend)
-    path: str = "m/44'/1'/0'/0/0"
+    path = "m/44'/535348'/0'"
 
     rapdu = client.get_public_key(path=path)
     _, public_key, _, _ = unpack_get_public_key_response(rapdu.data)
 
-    transaction = Transaction(
-        nonce=1,
-        coin="CRAB",
-        value=666,
-        to="de0b295669a9fd93d5f28d9ec85e40f4cb697bae",
-        memo=("This is a very long memo. "
-              "It will force the app client to send the serialized transaction to be sent in chunk. "
-              "As the maximum chunk size is 255 bytes we will make this memo greater than 255 characters. "
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.")
-    ).serialize()
-    
-    # Enable display of transaction memo (NBGL devices only)
-    if not firmware.device.startswith("nano"):
-        navigator.navigate([NavInsID.USE_CASE_HOME_SETTINGS,
-                            NavIns(NavInsID.TOUCH, (200, 113)),
-                            NavInsID.USE_CASE_SUB_SETTINGS_EXIT],
-                            screen_change_before_first_instruction=False,
-                            screen_change_after_last_instruction=False)
+    transaction=("looongtx" * 100).encode('utf-8')
 
     # Send the sign device instruction.
     # As it requires on-screen validation, the function is asynchronous.
     # It will yield the result when the navigation is done
     with client.sign_tx(path=path, transaction=transaction):
         # Validate the on-screen request by performing the navigation appropriate for this device
-        scenario_navigator.review_approve()
+        navigator.navigate_and_compare(
+            instructions=[NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK, NavInsID.BOTH_CLICK]
+            , timeout=10
+            , path=scenario_navigator.screenshot_path
+            , test_case_name="test_sign_tx_long_tx"
+            , screen_change_before_first_instruction=False
+            , screen_change_after_last_instruction=False
+        )
 
     response = client.get_async_response().data
-    _, der_sig, _ = unpack_sign_tx_response(response)
-    assert check_signature_validity(public_key, der_sig, transaction)
+    der_sig_len, der_sig, _ = unpack_sign_tx_response(response)
+    assert der_sig_len == 64
 
 
 # Transaction signature refused test
