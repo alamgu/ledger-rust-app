0337176e6269da629dc818328ec59c454b969356
Divam <dfordivam@gmail.com>
2024-10-31 12:53:49 +0900

    wip: check_sig_valid

--- ragger-tests/utils.py
+++ ragger-tests/utils.py
@@ -1,8 +1,8 @@
 from pathlib import Path
+from hashlib import blake2b
 from hashlib import sha256
-from sha3 import keccak_256
 
-from ecdsa.curves import SECP256k1
+from ecdsa.curves import Ed25519
 from ecdsa.keys import VerifyingKey
 from ecdsa.util import sigdecode_der
 
@@ -14,10 +14,9 @@ ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
 def check_signature_validity(public_key: bytes, signature: bytes, message: bytes) -> bool:
     pk: VerifyingKey = VerifyingKey.from_string(
         public_key,
-        curve=SECP256k1,
-        hashfunc=sha256
+        valid_encodings=['compressed'],
+        curve=Ed25519
     )
     return pk.verify(signature=signature,
                      data=message,
-                     hashfunc=keccak_256,
-                     sigdecode=sigdecode_der)
+                     hashfunc=blake2b)
