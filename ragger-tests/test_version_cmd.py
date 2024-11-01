d0a6c2bad0057408eeb9378c68e906ad5a5b2dc2
Divam <dfordivam@gmail.com>
2024-11-01 17:18:29 +0900

    test version command

--- ragger-tests/test_version_cmd.py
+++ ragger-tests/test_version_cmd.py
@@ -4,13 +4,12 @@ from application_client.boilerplate_response_unpacker import unpack_get_version_
 
 # In this test we check the behavior of the device when asked to provide the app version
 def test_version(backend):
-    
+
     with open("./rust-app/Cargo.toml", "rb") as f:
         data = tomli.load(f)
-    version = tuple(map(int, data['package']['version'].split('.')))
+    version = (tuple(map(int, data['package']['version'].split('.'))), "alamgu example")
     # Use the app interface instead of raw interface
     client = BoilerplateCommandSender(backend)
     # Send the GET_VERSION instruction
-    rapdu = client.get_version()
-    # Use an helper to parse the response, assert the values
-    assert unpack_get_version_response(rapdu.data) == (version)
+    response = client.get_app_and_version()
+    assert response == (version)
